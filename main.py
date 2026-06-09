37da86c3d489cc205729274ee9adf8a00e7acad2
Christian Frank <cabaladifrank@gmail.com>
2026-06-08 23:20:17 -0300

    Melhora formatacao do menu

--- main.py
+++ main.py
@@ -2,18 +2,20 @@ from notas import ler_nota, listar_notas, salvar_nota
 
 
 def criar_nova_nota():
+    print()
     titulo = input("Titulo da nota: ")
     conteudo = input("Conteudo da nota: ")
 
     try:
         caminho = salvar_nota(titulo, conteudo)
     except FileExistsError as erro:
-        print(f"Erro: {erro}")
+        print(f"\nErro: {erro}")
     else:
-        print(f"Nota salva com sucesso em: {caminho}")
+        print(f"\nNota salva com sucesso em: {caminho}")
 
 
 def mostrar_notas():
+    print()
     notas = listar_notas()
 
     if not notas:
@@ -26,22 +28,30 @@ def mostrar_notas():
 
 
 def mostrar_conteudo_nota():
+    print()
     nome_arquivo = input("Nome do arquivo da nota: ")
 
     try:
         conteudo = ler_nota(nome_arquivo)
     except FileNotFoundError as erro:
-        print(f"Erro: {erro}")
+        print(f"\nErro: {erro}")
     else:
+        print()
         print(conteudo)
+
+
+def mostrar_menu():
+    print()
+    print("=== Organizador de Notas ===")
+    print("1 - Criar nova nota")
+    print("2 - Listar notas")
+    print("3 - Ler nota")
+    print("4 - Sair")
 
 
 def main():
     while True:
-        print("1 - Criar nova nota")
-        print("2 - Listar notas")
-        print("3 - Ler nota")
-        print("4 - Sair")
+        mostrar_menu()
 
         opcao = input("Escolha uma opcao: ")
 
@@ -52,10 +62,10 @@ def main():
         elif opcao == "3":
             mostrar_conteudo_nota()
         elif opcao == "4":
-            print("Saindo do programa.")
+            print("\nSaindo do programa.")
             break
         else:
-            print("Opcao invalida.")
+            print("\nOpcao invalida.")
 
 
 if __name__ == "__main__":
